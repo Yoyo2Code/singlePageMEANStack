d43eb038fe98147b957c5eb090a2027ccbc20794
Yoseph Anderson <yrganderson@gmail.com>
2016-08-12 21:46:30 -0600

    angular create * api call for create * comments

--- public/js/services/NerdService.js
+++ public/js/services/NerdService.js
@@ -4,6 +4,13 @@ angular.module('NerdService', []).factory('Nerd', ['$http', function($http) {
         // call to get all nerds
         get : function() {
             return $http.get('/api/nerds');
+        },
+
+
+                // these will work when more API routes are defined on the Node side of things
+        // call to POST and create a new nerd
+        create : function(nerdData) {
+            return $http.post('/api/nerds', nerdData);
         }
     }
 
