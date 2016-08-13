6d8fe3e1a7654745e67a82bac3e6af742d0673c4
Yoseph Anderson <yrganderson@gmail.com>
2016-08-12 21:47:07 -0600

    delete api * angular delete api * ability to delete a nerd

--- public/js/services/NerdService.js
+++ public/js/services/NerdService.js
@@ -11,6 +11,11 @@ angular.module('NerdService', []).factory('Nerd', ['$http', function($http) {
         // call to POST and create a new nerd
         create : function(nerdData) {
             return $http.post('/api/nerds', nerdData);
+        },
+
+        // call to DELETE a nerd
+        delete : function(id) {
+            return $http.delete('/api/nerds/' + id);
         }
     }
 
